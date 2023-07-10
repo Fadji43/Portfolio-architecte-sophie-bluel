9ae7680c5c8ef4b2bb89eb410ee32ea82984d1f4
Fanny <lakrame@hotmail.com>
2023-07-10 11:26:37 +0200

    debut de la modale

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -46,3 +46,26 @@ const filterButtons = document.querySelectorAll('.filtres');
   })
 
 
+// affichage du mode éditeur avec éléments à masquer ou faire apparaitre
+const modeEditeur = document.getElementById('#editorMode')
+const logoModifier = document.querySelector('modifier')
+if (isAdmin) {
+  modeEditeur.style.display = 'block';
+  logoModifier.style.display = 'block';
+}
+else {
+  modeEditeur.style.display = 'none';
+  logoModifier.style.display = 'none';
+}
+
+function (isAdmin)
+
+//création de la modale
+//modal1
+//modal2
+//modal3
+
+//addEventlisteners au clique de title_Project et caption_photo
+// au clique du bouton ajoutre photo chnagement de modale
+//charger la photo et insérer texte et catégorie
+// valider les informations et fermture de la modale
